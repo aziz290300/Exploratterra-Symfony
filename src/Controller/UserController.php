0c152fd37fe02ff0cbb24e76e669e4f6ceabf418
unknown <aziz.abidi@esprit.tn>
2023-12-06 13:08:55 +0000

    explora

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+
 class UserController extends AbstractController
 {
     private $passwordEncoder;
